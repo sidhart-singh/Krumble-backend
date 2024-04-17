a0cc0dc30f430a9d1431b682fb3f168491703073
Sidhart Singh <isidhartsingh@gmail.com>
2024-04-17 17:26:29 +0530

    update db connection

--- src/db/db.js
+++ src/db/db.js
@@ -3,15 +3,15 @@ const pg = require("pg");
 require("dotenv").config();
 
 const db = new Sequelize(
-  // process.env.DB_URL ||
-  "postgres://postgres:password@localhost:5432/krumble_db_test",
+  process.env.DB_URL ||
+    "postgres://postgres:password@localhost:5432/krumble_db_test",
   {
     dialect: "postgres",
-    dialectModule: pg,
+    // dialectModule: pg,
     // dialectOptions: {
-    // ssl: {
-    //   require: true,
-    // },
+    //   ssl: {
+    //     require: true,
+    //   },
     // },
   }
 );
